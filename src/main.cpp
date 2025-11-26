5c7137a8740fa9d7ec19499a1021d70f19e43d8b
Royyan Dzakiy <r.dzakiy@lmesh.eu>
2025-11-26 20:31:49 +0700

    remove vcpkg

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,7 @@
-#include <iostream>
-#include <spdlog/spdlog.h>
+#include <print>
 
-int main()
-{
-	// Set the global log level to info to see messages from info and above.
-	spdlog::set_level(spdlog::level::info);
+int main() {
+    std::println("Hello, World!");
 
-	// Log a few messages at different levels to demonstrate spdlog.
-	spdlog::info("Welcome to the spdlog console application!");
-	spdlog::warn("This is a warning message.");
-	spdlog::error("This is an error message.");
-	spdlog::critical("This is a critical message!");
-
-	// Log a formatted message using the "fmt" syntax included with spdlog.
-	spdlog::info("The answer is {}", 42);
-
-	return 0;
+    return 0;
 }
